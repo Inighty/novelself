174ba9d3b5daa3a44eb4134fa3ed268c5317999b
Star Ins <Star Ins>
2016-10-20 23:08:51 +0800

    obtain entity of book from BQG

---
+++ src/main/java/com/novelspider/application/App.java
@@ -0,0 +1,14 @@
+package com.novelspider.application;
+
+import com.novelspider.crawler.*;
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
